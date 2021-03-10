f922fd0f72cc31f9c2b25aa3a8e0fd87476f968f
Timothy Small <smalls.online@icloud.com>
2021-03-09 19:37:10 -0500

    Updated script
    Multiple changes have been made:
    - Fixed the parameter declarations and the corresponding parameter set names.
    - Added parameters:
    	- CopyAudioStream
    	- AudioBitrateKb
    	- VideoConstantRateFactor
    	- VideoEncodingSpeed
    - Made the 'ffmpeg' argument list a dynamically generated list based off of the parameters provided.
    - Changed the ShouldProcess() parameters from being both ShouldProcess(target, action) to just ShouldProcess(target).
    - Added comments

--- Convert-VideoToMP4.ps1
+++ Convert-VideoToMP4.ps1
@@ -1,34 +1,56 @@
-[CmdletBinding(SupportsShouldProcess)]
+[CmdletBinding(SupportsShouldProcess, DefaultParameterSetName = "Default")]
 param(
-    [Parameter(Position = 0, Mandatory)]
-    [Parameter(ParameterSetName = "Default")]
-    [Parameter(ParameterSetName = "DefaultAndSubtitles")]
+    [Parameter(Position = 0, Mandatory, ParameterSetName = "Default")]
+    [Parameter(Position = 0, Mandatory, ParameterSetName = "BurnInSubtitles")]
     [string[]]$VideoFile,
-    [Parameter(Position = 1, Mandatory)]
-    [Parameter(ParameterSetName = "Default")]
-    [Parameter(ParameterSetName = "DefaultAndSubtitles")]
+    [Parameter(Position = 1, Mandatory, ParameterSetName = "Default")]
+    [Parameter(Position = 1, Mandatory, ParameterSetName = "BurnInSubtitles")]
     [string]$OutputDir,
-    [Parameter(Position = 2)]
-    [Parameter(ParameterSetName = "Default")]
-    [Parameter(ParameterSetName = "DefaultAndSubtitles")]
+    [Parameter(Position = 2, ParameterSetName = "Default")]
+    [Parameter(Position = 2, ParameterSetName = "BurnInSubtitles")]
     [int]$VideoStream = 0,
-    [Parameter(Position = 3)]
-    [Parameter(ParameterSetName = "Default")]
-    [Parameter(ParameterSetName = "DefaultAndSubtitles")]
+    [Parameter(Position = 3, ParameterSetName = "Default")]
+    [Parameter(Position = 3, ParameterSetName = "BurnInSubtitles")]
     [int]$AudioStream = 0,
-    [Parameter(Position = 4)]
-    [Parameter(ParameterSetName = "DefaultAndSubtitles")]
+    [Parameter(Position = 4, ParameterSetName = "Default")]
+    [Parameter(Position = 4, ParameterSetName = "BurnInSubtitles")]
+    [switch]$CopyAudioStream,
+    [Parameter(Position = 5, ParameterSetName = "Default")]
+    [Parameter(Position = 5, ParameterSetName = "BurnInSubtitles")]
+    [int]$AudioBitrateKb = 640,
+    [Parameter(Position = 6, ParameterSetName = "Default")]
+    [Parameter(Position = 6, ParameterSetName = "BurnInSubtitles")]
+    [int]$VideoConstantRateFactor = 16,
+    [Parameter(Position = 7, ParameterSetName = "Default")]
+    [Parameter(Position = 7, ParameterSetName = "BurnInSubtitles")]
+    [ValidateSet(
+        "ultrafast",
+        "superfast",
+        "veryfast",
+        "faster",
+        "fast",
+        "medium",
+        "slow",
+        "slower",
+        "veryslow"
+    )]
+    [string]$VideoEncodingSpeed = "medium",
+    [Parameter(Position = 8, ParameterSetName = "BurnInSubtitles")]
     [int]$SubtitleStream
 )
 
 process {
-    foreach ($videoFileItem in $VideoFile) {
+    foreach ($videoFileItem in $VideoFile) { #Process through each input file provided
+
+        #Try to resolve the path of the video file and the output directory. If resolving the path fails, then terminate the script.
         $videoFilePath = (Resolve-Path -Path $videoFileItem -ErrorAction "Stop").Path
         $outputDirPath = (Resolve-Path -Path $OutputDir -ErrorAction "Stop").Path
 
+        #Get the FileInfo and DirectoryInfo objects of the video file and output directory.
         $videoFileItem = Get-Item -Path $videoFilePath
         $outputDirItem = Get-Item -Path $outputDirPath
 
+        #Check to see if the output directory supplied is actually a directory, if it isn't then throw a terminating error.
         switch ($outputDirItem.Attributes -eq [System.IO.FileAttributes]::Directory) {
             $false {
                 $PSCmdlet.ThrowTerminatingError(
@@ -42,48 +64,92 @@ process {
             }
         }
 
+        #Generate the file name of the output file and join it with the path to the output directory.
         $outputFilePath = Join-Path -Path $outputDirItem.FullName -ChildPath "$($videoFileItem.BaseName).mp4"
 
-        $ffmpegArgs = $null
+        # -- Start building the argument list for 'ffmpeg' --
+
+        #Create a list object and supply the initial arguments to start with.
+        $ffmpegArgs = [System.Collections.Generic.List[string]]::new(
+            [string[]]@(
+                "-hide_banner",
+                "-v error",
+                "-stats",
+                "-i `"$($videoFileItem.FullName)`""
+            )
+        )
+
+        #Determine if the parameter set is 'BurnInSubtitles' or 'Default'. Then add the necessary arguments to the list.
         switch ($PSCmdlet.ParameterSetName) {
-            "DefaultAndSubtitles" {
-                Write-Verbose "Subtitles will be burned into the video stream."
-                $ffmpegArgs = @(
-                    "-i `"$($videoFileItem.FullName)`"",
-                    "-filter_complex `"[$($VideoStream):v:0][$($SubtitleStream):s:0]overlay[v]`"",
-                    "-map `"[v]`"",
-                    "-map $($AudioStream):a:0",
-                    "-c:v libx265",
-                    "-preset fast",
-                    "-crf 16",
-                    "-profile:v main10",
-                    "-c:a libfdk_aac",
-                    "-b:a 640k",
-                    "-tag:v hvc1",
-                    "`"$($outputFilePath)`""
+            "BurnInSubtitles" {
+                Write-Warning "Subtitles will be burned into the video stream."
+                $ffmpegArgs.AddRange(
+                    [string[]]@(
+                        "-filter_complex `"[$($VideoStream):v:0][$($SubtitleStream):s:0]overlay[v]`"",
+                        "-map `"[v]`"",
+                        "-c:v libx265",
+                        "-preset $($VideoEncodingSpeed)",
+                        "-crf $($VideoConstantRateFactor)",
+                        "-x265-params log-level=error",
+                        "-profile:v main10"
+                    )
                 )
                 break
             }
 
             Default {
-                Write-Verbose "Subtitles will not be processed."
-                $ffmpegArgs = @(
-                    "-i `"$($videoFileItem.FullName)`"",
-                    "-map $($VideoStream):v:0",
-                    "-map $($AudioStream):a:0",
-                    "-c:v libx265",
-                    "-preset fast",
-                    "-crf 16",
-                    "-profile:v main10",
-                    "-c:a libfdk_aac",
-                    "-b:a 640k",
-                    "-tag:v hvc1",
-                    "`"$($outputFilePath)`""
+                Write-Warning "Subtitles will not be processed."
+                $ffmpegArgs.AddRange(
+                    [string[]]@(
+                        "-map $($VideoStream):v:0",
+                        "-c:v libx265",
+                        "-preset $($VideoEncodingSpeed)",
+                        "-crf $($VideoConstantRateFactor)",
+                        "-x265-params log-level=error",
+                        "-profile:v main10"
+                    )
                 )
                 break
             }
         }
 
+        #Determine if the audio stream will be copied or re-encoded if the 'CopyAudioStream' switch parameter is provided.
+        switch ($CopyAudioStream) {
+            $true {
+                Write-Warning "Audio stream will be copied."
+                $ffmpegArgs.AddRange(
+                    [string[]]@(
+                        "-map $($AudioStream):a:0",
+                        "-c:a copy"
+                    )
+                )
+                break
+            }
+
+            Default {
+                Write-Warning "Audio stream will be re-encoded to $($AudioBitrateKb) Kb/s AAC."
+                $ffmpegArgs.AddRange(
+                    [string[]]@(
+                        "-map $($AudioStream):a:0",
+                        "-c:a libfdk_aac",
+                        "-b:a $($AudioBitrateKb)k"
+                    )
+                )
+                break
+            }
+        }
+
+        # Add the last arguments to the list.
+        $ffmpegArgs.AddRange(
+            [string[]]@(
+                "-tag:v hvc1",
+                "`"$($outputFilePath)`""
+            )
+        )
+
+        # -- End building the argument list for 'ffmpeg' --
+
+        #Create a hashtable of the parameters to pass to 'Start-Process'
         $ffmpegSplat = @{
             "FilePath"     = "ffmpeg";
             "ArgumentList" = $ffmpegArgs;
@@ -91,8 +157,8 @@ process {
             "NoNewWindow"  = $true;
         }
 
-
-        if ($PSCmdlet.ShouldProcess("ffmpeg", "$($ffmpegArgs -join " ")")) {
+        #Begin the 'ffmpeg' process
+        if ($PSCmdlet.ShouldProcess("ffmpeg $($ffmpegArgs -join " ")")) {
             Start-Process @ffmpegSplat
         }
     }
